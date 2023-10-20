8047bbef18d34b65876d62549dba503602a7a505
Sandeep Singh <sandeepgaurdec13@gmail.com>
2023-10-20 21:14:58 +0530

    Overloading

---
+++ src/overloading/A.java
@@ -0,0 +1,10 @@
+package src.overloading;
+
+public class A {
+    public void print() {
+        System.out.println("Method called with out Argument");
+    }
+    public void print(String name){
+        System.out.println("Method called with Argument");
+    }
+}
---
+++ src/overloading/Main.java
@@ -0,0 +1,9 @@
+package src.overloading;
+
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+        a.print();
+        a.print("Sandeep");
+    }
+}
